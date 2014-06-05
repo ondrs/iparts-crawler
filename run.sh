c0775b21e6bc21bd3315ffc5b6210650fbffb09c
ondrs <plsek.o@gmail.com>
2014-06-05 19:08:13 +0200

    half

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ node app/run --start --url http://www.iparts.pl/auto/iveco,daily-iii-bus-od-2006
 node app/run --start --url http://www.iparts.pl/auto/iveco,daily-iii-nadwozie-pelne-kombi-od-2006,55-9525.html &
 node app/run --start --url http://www.iparts.pl/auto/iveco,daily-iii-platforma-podwozie-od-2006,55-9527.html &
 node app/run --start --url http://www.iparts.pl/auto/iveco,daily-iii-wywrotka-od-2006,55-9526.html &
-node app/run --start --url http://www.iparts.pl/auto/iveco,eurocargo-od-1991,55-3421.html &
-node app/run --start --url http://www.iparts.pl/auto/iveco,stralis-od-2002,55-4927.html &
-node app/run --start --url http://www.iparts.pl/auto/iveco,trakker-od-2004,55-5483.html &
+#node app/run --start --url http://www.iparts.pl/auto/iveco,eurocargo-od-1991,55-3421.html &
+#node app/run --start --url http://www.iparts.pl/auto/iveco,stralis-od-2002,55-4927.html &
+#node app/run --start --url http://www.iparts.pl/auto/iveco,trakker-od-2004,55-5483.html &
 wait
 echo OK
